8ce52200432a5eee4e9a56a1a556b6bc3adc428e
vaishnavi <vaishnavijha006@gmail.com>
2026-07-28 20:42:43 +0530

    updated

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -2,7 +2,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabaseUrl = https://viyssnrvtpoccletcqgy.supabase.co
+const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpeXNzbnJ2dHBvY2NsZXRjcWd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNjA0NjIsImV4cCI6MjEwMDczNjQ2Mn0.8uZ3j5OMDiaaXrUBgON_j4wiCpREEO6v4KqeY7LUrss
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
